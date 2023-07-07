d261fa9c231ce6abda72a582159217223342afdd
rish-99 <risinimethmini@gmail.com>
2023-07-07 21:58:58 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var Typed = new Typed('.text', {
+    strings: ["Front-end Developer", "Web Designer", "Graphic Designer", "Photographer", "Volunteer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
